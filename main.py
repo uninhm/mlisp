faa478e4b86b3720750bbd163ff194ca185a7af0
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-11-30 19:15:55 -0300

    Improve main.py

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
+from sys import argv, setrecursionlimit
+
 from lexer import Lexer
 from parser import Parser, Scope
 from interpreter import Interpreter
 
-from sys import argv, setrecursionlimit
-
-def main():
-  lexer = Lexer()
-  parser = Parser()
-  interpreter = Interpreter()
-  tokens = []
-  while (line := input()) != 'exit':
-    tokens += lexer.make_tokens(line)
-    op = parser.parse(tokens)
-    if op == 'WFMT':
-      continue
-    if op is not None:
-      print(interpreter.run(op))
+def main_interactive():
     tokens = []
+    while (line := input('>>> ')) != 'exit':
+        tokens += lexer.make_tokens(line)
+        for op in parser.parse(tokens):
+            if op == 'WFMT':
+                continue
+            if op is not None:
+                print(interpreter.run(op, global_scope))
+            tokens = []
+
+def main_file(file_name):
+    with open(file_name, 'r') as f:
+        for instruction in parser.parse(lexer.make_tokens(f.read())):
+            print(interpreter.run(instruction, global_scope))
 
 if __name__ == '__main__':
-  setrecursionlimit(1000000000)
-  if len(argv) > 1:
-    with open(argv[1]) as f:
-      lexer = Lexer()
-      parser = Parser()
-      interpreter = Interpreter()
-      global_scope = Scope()
-      global_scope.content = {
+    setrecursionlimit(1000000000)
+    lexer = Lexer()
+    parser = Parser()
+    interpreter = Interpreter()
+    global_scope = Scope()
+    global_scope.content = {
         'true': True,
         'false': False,
-      }
-      for op in '+-=<>*/':
+    }
+    for op in '+-=<>*/':
         global_scope.add(op, op)
-      for op in ('div', 'mod', 'def', 'if', 'cond', 'or', 'and', 'not'):
+    for op in ('div', 'mod', 'def', 'if', 'cond', 'or', 'and', 'not'):
         global_scope.add(op, op)
-      
-      for instruction in parser.parse(lexer.make_tokens(f.read())):
-        print(interpreter.run(instruction, global_scope))
-  else:
-    main()
+
+    if len(argv) > 1:
+        main_file(argv[1])
+    else:
+        main_interactive()
