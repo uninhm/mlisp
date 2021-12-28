0ad0367c9bee3165f8ce91059bb6f04d320c9d45
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-12-28 19:05:15 -0300

    use negative numbers in example

--- examples/readfromfile.lisp
+++ examples/readfromfile.lisp
@@ -3,7 +3,7 @@
 (def buf-size 256)
 (reserve buf buf-size)
 
-(var fd:int (openat (- 0 100) "testfile.txt" 0 440))
+(var fd:int (openat (- 100) "testfile.txt" 0 440))
 (read fd buf buf-size)
 
 (puts buf buf-size)
